f7dd29d2c818c5dde93f171814cf5fca840c0f65
BjarkeHJ <bjarke_holm@hotmail.com>
2025-11-23 23:13:18 +0100

    tf frame transforms

--- src/gz_px4/include/tf_pub.hpp
+++ src/gz_px4/include/tf_pub.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "px4_msgs/msg/vehicle_odometry.hpp"
+#include "px4_ros_com/frame_transforms.h"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "tf2_ros/transform_broadcaster.h"
 
--- src/gz_px4/src/tf_pub.cpp
+++ src/gz_px4/src/tf_pub.cpp
@@ -19,28 +19,47 @@ PxOdomToTf::PxOdomToTf() : Node("px4_odom_to_tf")
 
 void PxOdomToTf::odomCallback(const px4_msgs::msg::VehicleOdometry::SharedPtr msg)
 {
+    using namespace px4_ros_com::frame_transforms;
+
+    // --- Position: NED -> ENU ---
+    // PX4: position[0]=N, [1]=E, [2]=D  (NED)
+    Eigen::Vector3d ned_pos(msg->position[0],
+                            msg->position[1],
+                            msg->position[2]);
+
+    // ROS: x=E, y=N, z=U  (ENU)
+    Eigen::Vector3d enu_pos = ned_to_enu_local_frame(ned_pos);
+
+    // --- Orientation: aircraft(FRD)->NED  ->  base_link(FLU)->ENU ---
+
+    // PX4 stores quaternion as Hamilton (w, x, y, z) from body(FRD) -> world(NED)
+    Eigen::Quaterniond q_px4 =
+        utils::quaternion::array_to_eigen_quat(msg->q);  // (w,x,y,z)
+
+    // Convert aircraft(FRD) frame to ROS base_link(FLU) frame
+    Eigen::Quaterniond q_baselink_ned =
+        aircraft_to_baselink_orientation(q_px4);
+
+    // Convert world frame NED -> ENU
+    Eigen::Quaterniond q_baselink_enu =
+        ned_to_enu_orientation(q_baselink_ned);
+
+    // --- Pack into TF message ---
     geometry_msgs::msg::TransformStamped tf_msg;
-
     tf_msg.header.stamp = this->get_clock()->now();
-    tf_msg.header.frame_id = "world";        // parent
-    tf_msg.child_frame_id  = "base_link";  // child
+    tf_msg.header.frame_id = "world";      // ENU world
+    tf_msg.child_frame_id  = "base_link";  // FLU body
 
-    // tf_msg.transform.translation.x = msg->position[0];
-    // tf_msg.transform.translation.y = msg->position[1];
-    // tf_msg.transform.translation.z = msg->position[2];
+    tf_msg.transform.translation.x = enu_pos.x();
+    tf_msg.transform.translation.y = enu_pos.y();
+    tf_msg.transform.translation.z = enu_pos.z();
 
-    tf_msg.transform.translation.x = msg->position[1];
-    tf_msg.transform.translation.y = msg->position[0];
-    tf_msg.transform.translation.z = -msg->position[2];
-
-    tf_msg.transform.rotation.x = msg->q[0];
-    tf_msg.transform.rotation.y = msg->q[1];
-    tf_msg.transform.rotation.z = msg->q[2];
-    tf_msg.transform.rotation.w = msg->q[3];
+    tf_msg.transform.rotation.w = q_baselink_enu.w();
+    tf_msg.transform.rotation.x = q_baselink_enu.x();
+    tf_msg.transform.rotation.y = q_baselink_enu.y();
+    tf_msg.transform.rotation.z = q_baselink_enu.z();
 
     tf_broadcaster_->sendTransform(tf_msg);
-
-    std::cout << "PUBLISHED TRANSFORM!" << std::endl;
 }
 
 }
